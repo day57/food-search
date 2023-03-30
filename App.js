38e03fa59e82033ec33348d5ee62d6734897d1b3
day57 <theday057@gmail.com>
2023-03-30 02:27:26 -0700

    v1

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const navigator = createStackNavigator(
   {
-    Search: SearchScreen
-  }, 
+    Search: SearchScreen,
+    ResultsShow: ResultsShowScreen,
+  },
   {
-    //Default Route
     initialRouteName: 'Search',
-    // options for every screen
-    defaultNavigationOptions : {
-      title: 'Business Search'
-    }
+    defaultNavigationOptions: {
+      title: 'Business Search',
+    },
   }
-)
+);
 
-
-export default createAppContainer(navigator)
+export default createAppContainer(navigator);
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
-    const [ term, setTerm ] = useState('');
-    const [ results, setResults ] = useState([]);
-    const [errorMessage, setErroMessage ] = useState('');
+const SearchScreen = ({ navigation }) => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
 
-    // const response is await get '/search'
-    const searchApi = async (searchTerm) => {
-        try {    
-            //waiting for the data , then when we get the data we assing it to the response variable
-            const response = await yelp.get('/search',{
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            
-            //we are calling our useState as setResults
-            // the response comes with a .data property we can use to get the data back from the api  
-            setResults(response.data.businesses)
-    
-        } catch (err) {
-            setErroMessage('Something went wrong ');
-        }
-    }
+  return (
+    <>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+          navigation={navigation}
+        />
+        <ResultsList
+          navigation={navigation}
+          results={filterResultsByPrice('$$')}
+          title="Bit Pricier"
+        />
+        <ResultsList
+          navigation={navigation}
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
+  );
+};
 
-    
+const styles = StyleSheet.create({});
 
-    return (
-        <View>
-            <SearchBar 
-                term={term} 
-                onTermChange={setTerm}
-                onTermSubmit={() => searchApi(term)}
-            />
-            { errorMessage ? <Text>{errorMessage}</Text> : null }
-          
-            <Text>We have found: {results.length} results </Text>
-        </View>
-    )
-}
-
-export default SearchScreen
+export default SearchScreen;
